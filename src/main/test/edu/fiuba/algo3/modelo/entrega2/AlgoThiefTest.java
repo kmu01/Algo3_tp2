6711e9fca32dec4b3827bd937e00321d605cf707
kmu01 <kmufederico@gmail.com>
2021-12-05 20:54:54 -0300

    Se cargan ciudades, ladrones y tesoros

---
+++ src/main/test/edu/fiuba/algo3/modelo/entrega2/AlgoThiefTest.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.modelo.entrega2;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlgoThiefTest {
+
+    @Test
+    public void losDatosSeCarganCorrectamente() {
+        Partida partida = new Partida();
+    }
+
+
+}
