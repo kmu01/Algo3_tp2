762e7f8f1a09de1324f8172165ff45576026505c
Nicolas <nicolasmmartino@hotmail.com>
2021-12-14 11:51:35 -0300

    agrego escena vacia

--- src/main/java/edu/fiuba/algo3/controllers/MenuDeInicioController.java
+++ src/main/java/edu/fiuba/algo3/controllers/MenuDeInicioController.java
@@ -1,4 +1,21 @@
 package edu.fiuba.algo3.controllers;
 
-public class MenuDeInicioController {
+import edu.fiuba.algo3.App;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuDeInicioController implements Initializable {
+    @FXML public AnchorPane bc;
+    public void comenzar(){
+        CargadorDeEscena.cargarEscena("/fxml/pedirCantidadDeArrestos.fxml", App.devolverEscena(),"Cantidad de arrestos");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
